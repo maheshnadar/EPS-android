e89d358a06cf08c756125d39ef86572110dd8527
maheshnadar <mahesh.nadar@wohlig.com>
2018-08-31 14:45:03 +0530

    intent generation spinner fix

--- app/src/main/java/com/example/eps/eps/MainActivity.java
+++ app/src/main/java/com/example/eps/eps/MainActivity.java
@@ -13,36 +13,36 @@ import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+
 //import com.example.eps.eps.ApiCall;
 
-public class MainActivity extends AppCompatActivity  implements AdapterView.OnItemSelectedListener{
+public class MainActivity extends AppCompatActivity  {
 
     private boolean userIsInteracting;
-    String[] bankNames={"BOI","SBI","HDFC","PNB","OBC"};
-    String[] branchName={"Mumbai","delhi","chennai"};
-    String[] regionName={"worli","sion","dadar"};
+   // String[] bankNames={"BOI","SBI","HDFC","PNB","OBC"};
+    //String[] branchName={"Mumbai","delhi","chennai"};
+  //  String[] regionName={"worli","sion","dadar"};
+    ArrayList<String> bankNames,branchName,regionName;
     ConstraintLayout detailsLayout;
     Spinner bankSpinner,branchSpinner,regionSpinner;
     Button searchBtn,approvalBtn;
     ScrollView detailScroll;
     ProgressBar loader;
+    ArrayAdapter bankAdapter,branchAdapter,regionAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initializeUI();
-
+        createlist();
+        initializeAdapter();
 //
-        ArrayAdapter bankAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,bankNames);
-        bankAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        bankSpinner.setAdapter(bankAdapter);
-        bankSpinner.setSelection(0,true);
-        bankSpinner.setSelected(false);
-        bankSpinner.setOnItemSelectedListener(this);
-//        branchSpinner.setAdapter(null);
-//        regionSpinner.setAdapter(null);
-
-
+        bankSpinner.setOnItemSelectedListener(bank_listener);
+        branchSpinner.setOnItemSelectedListener(branch_listener);
+        regionSpinner.setOnItemSelectedListener(region_listener);
     }
 
     public void initializeUI(){
@@ -55,6 +55,111 @@ public class MainActivity extends AppCompatActivity  implements AdapterView.OnIt
         detailScroll=(ScrollView) findViewById(R.id.detailscroll);
         loader=findViewById(R.id.loader);
     }
+    public void createlist(){
+        bankNames = new ArrayList<>();
+        branchName =  new ArrayList<>();
+        regionName=  new ArrayList<>();
+        bankNames.add("select Bank");
+        bankNames.add("SBI");
+        bankNames.add("HDFC");
+        bankNames.add("KOTAK");
+//        branchName.add("select branch");
+//        regionName.add("select region");
+    }
+    public void initializeAdapter(){
+    bankAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,bankNames);
+    bankAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    bankSpinner.setAdapter(bankAdapter);
+//    bankSpinner.setSelection(0,true);
+//    bankSpinner.setSelected(false);
+//    bankSpinner.setOnItemSelectedListener(this);
+//        branchSpinner.setAdapter(null);
+//        regionSpinner.setAdapter(null);
+    branchAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,branchName);
+    branchAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    branchSpinner.setAdapter(branchAdapter);
+
+    regionAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,regionName);
+    regionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    regionSpinner.setAdapter(regionAdapter);
+}
+
+    private AdapterView.OnItemSelectedListener bank_listener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            Log.i("main","inside bank listener");
+            hideSearchResult();
+            if (position > 0) {
+                String selectedbank= (String) bankSpinner.getItemAtPosition(position);
+                ArrayList<String> tempBranch = new ArrayList<>();
+                tempBranch.add("select branch");
+                tempBranch.add("DELHI");
+                tempBranch.add("MUMBAI");
+                tempBranch.add("CHENNAI");
+                branchAdapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_spinner_item,tempBranch);
+                branchAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                branchSpinner.setAdapter(branchAdapter);
+
+                ArrayList<String> tempRegion = new ArrayList<>();
+                regionAdapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_spinner_item,tempRegion);
+                regionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                regionSpinner.setAdapter(regionAdapter);
+            }
+            else{
+//                ArrayList<String> tempBranch = new ArrayList<>();
+//               // tempBranch.add("select branch");
+//                branchAdapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_spinner_item,tempBranch);
+//                branchAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//                branchSpinner.setAdapter(branchAdapter);
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
+    private AdapterView.OnItemSelectedListener branch_listener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            Log.i("main","inside branch listener");
+            hideSearchResult();
+            if (position > 0) {
+
+                String selectedbranch= (String) bankSpinner.getItemAtPosition(position);
+                ArrayList<String> tempRegion = new ArrayList<>();
+                tempRegion.add("select branch");
+                tempRegion.add("sion");
+                tempRegion.add("matunga");
+                tempRegion.add("dadar");
+                regionAdapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_spinner_item,tempRegion);
+                regionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                regionSpinner.setAdapter(regionAdapter);
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
+    private AdapterView.OnItemSelectedListener region_listener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            Log.i("main","inside region listener");
+            if (position > 0) {
+
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
+
+
+
 
     @Override
     public void onUserInteraction() {
@@ -75,57 +180,70 @@ public class MainActivity extends AppCompatActivity  implements AdapterView.OnIt
 
 
     //Performing action onItemSelected and onNothing selected
-    @Override
-    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+//    @Override
+//    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+//
+//        if (!userIsInteracting) {
+//            Log.i("main","user interacting");
+//            return ;
+//        }
+//        Log.i("TAG", "Information level message"+arg0.getId()+"=="+R.id.bank_spinner+"__________"+ arg1+"------"+id);
+//
+//        if(arg0.getId()== R.id.bank_spinner){
+//Log.i("inside if","bank spinner"+R.id.bank_spinner+"---"+arg0.getId());
+//
+//           // regionSpinner.setAdapter(null);
+//        }
+//        else if(arg0.getId()== R.id.branch_spinner){
+//            Log.i("inside if","branch spinner"+R.id.branch_spinner+"---"+arg0.getId());
+//
+//        }
+//        else if(arg0.getId()== R.id.region_spinner){
+//            Log.i("inside if","region spinner"+R.id.region_spinner+"---"+arg0.getId());
+//searchBtn.setClickable(true);
+//
+//        }
+//        Toast.makeText(getApplicationContext(), bankNames[position], Toast.LENGTH_LONG).show();
+//    }
 
-        if (!userIsInteracting) {
-            Log.i("main","user interacting");
-            return ;
-        }
-        Log.i("TAG", "Information level message"+arg0.getId()+"=="+R.id.bank_spinner+"__________"+ arg1+"------"+id);
-
-        if(arg0.getId()== R.id.bank_spinner){
-Log.i("inside if","bank spinner"+R.id.bank_spinner+"---"+arg0.getId());
-            ArrayAdapter branchAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,branchName);
-            branchAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            branchSpinner.setAdapter(branchAdapter);
-            branchSpinner.setSelection(0,true);
-            branchSpinner.setSelected(false);
-            branchSpinner.setOnItemSelectedListener(this);
-           // regionSpinner.setAdapter(null);
-        }
-        else if(arg0.getId()== R.id.branch_spinner){
-            Log.i("inside if","branch spinner"+R.id.branch_spinner+"---"+arg0.getId());
-            ArrayAdapter regionAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,regionName);
-            regionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            regionSpinner.setAdapter(regionAdapter);
-            regionSpinner.setSelection(0,true);
-            regionSpinner.setSelected(false);
-            regionSpinner.setOnItemSelectedListener(this);
-        }
-        else if(arg0.getId()== R.id.region_spinner){
-            Log.i("inside if","region spinner"+R.id.region_spinner+"---"+arg0.getId());
-searchBtn.setClickable(true);
-
-        }
-        Toast.makeText(getApplicationContext(), bankNames[position], Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> arg0) {
-// TODO Auto-generated method stub
-
-    }
+//    @Override
+//    public void onNothingSelected(AdapterView<?> arg0) {
+//// TODO Auto-generated method stub
+//
+//    }
 
 
-    public void search(View view)
+    public void searchfn(View view)
     {
+
+     if (bankSpinner.getSelectedItemPosition()==0) {
+
+         Toast.makeText(getApplicationContext(),"Please Select Bank", Toast.LENGTH_LONG).show();
+         return;
+    }
+        if (branchSpinner.getSelectedItemPosition()==0) {
+
+            Toast.makeText(getApplicationContext(),"Please Select Branch", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (regionSpinner.getSelectedItemPosition()==0) {
+
+            Toast.makeText(getApplicationContext(),"Please Select region", Toast.LENGTH_LONG).show();
+            return;
+        }
         detailScroll.setVisibility(View.VISIBLE);
         approvalBtn.setVisibility(View.VISIBLE);
         // Do something in response to button click
+
+
+
     }
 
-    public void reset(){
+    public void detailsTogglefn(View view){
+
+    }
+
+    public void hideSearchResult(){
         detailScroll.setVisibility(View.INVISIBLE);
         approvalBtn.setVisibility(View.INVISIBLE);
     }
@@ -135,6 +253,8 @@ searchBtn.setClickable(true);
     public void hideLoader(){
         loader.setVisibility(View.INVISIBLE);
     }
+
+
 
     // event listener
 
